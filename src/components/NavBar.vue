12fa35c106bcc2954e88dd446f0a28a8d9c81005
KwonYG <andamirocll@gmail.com>
2019-07-19 00:57:51 +0900

    Add: NavBar.vue, user avatar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -22,7 +22,24 @@
     </v-navigation-drawer>
     <v-toolbar color="indigo" dark fixed app>
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>퍼펙트 매칭</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>notifications_none</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mail_outline</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-avatar :tile="tile" :size="36" color="grey lighten-4">
+          <img
+            src="https://vuetifyjs.com/apple-touch-icon-180x180.png"
+            alt="avatar"
+          />
+        </v-avatar>
+      </v-btn>
     </v-toolbar>
   </header>
 </template>
