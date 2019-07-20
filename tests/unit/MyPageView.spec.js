4326a3984396a5072deda15e7fad5040d035cad3
KwonYG <andamirocll@gmail.com>
2019-07-20 19:52:19 +0900

    Add: test code for MyPageView.vue

---
+++ tests/unit/MyPageView.spec.js
@@ -0,0 +1,32 @@
+import { shallowMount } from "@vue/test-utils";
+import Vuetify from "vuetify";
+import Vue from "vue";
+
+import MyPageView from "../../src/views/MyPageView.vue";
+import MyInfo from "../../src/components/MyPageView/MyInfo.vue";
+import MyProjectList from "../../src/components/MyPageView/MyProjectList.vue";
+import MyProjectCard from "../../src/components/MyPageView/MyProjectCard.vue";
+
+describe('MyPageView.vue', () => {
+    let wrapper;
+    beforeEach(() => {
+        Vue.use(Vuetify);
+        wrapper = shallowMount(MyPageView)
+    });
+
+    test('renders a vue instance', () => {
+        expect(wrapper.isVueInstance()).toBe(true);
+    })
+
+    test('check if child MyInfo exists', () => {
+        expect(wrapper.contains(MyInfo)).toBe(true);
+    })
+
+    test('check if child MyProjectList', () => {
+        expect(wrapper.contains(MyProjectList)).toBe(true);
+    })
+
+    test('check if MyProjectList has child MyProjectCard', () => {
+        expect(shallowMount(MyProjectList).contains(MyProjectCard)).toBe(true);
+    })
+});
