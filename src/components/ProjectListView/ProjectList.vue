7895c25fd855c3d7d7f172d49ce66ed28aed177c
KwonYG <andamirocll@gmail.com>
2019-07-22 18:23:44 +0900

    Add: ProjectListView with related components

---
+++ src/components/ProjectListView/ProjectList.vue
@@ -0,0 +1,87 @@
+<template>
+  <v-container class="project_list_container">
+    <v-layout>
+      <v-flex xs12 sm4>
+        <drop-down :title="'지역'"></drop-down>
+      </v-flex>
+      <v-flex xs12 sm4>
+        <drop-down :title="'직군'"></drop-down>
+      </v-flex>
+    </v-layout>
+    <v-layout class="info_bar">
+      <v-flex>주최자</v-flex>
+      <v-flex xs5>프로젝트 명</v-flex>
+      <v-flex xs2>모집 마감일</v-flex>
+      <v-flex xs2>모집 공고일</v-flex>
+      <v-flex xs1>상태</v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex>
+        <ul class="project_list">
+          <li class="list_item">
+            <project-list-item></project-list-item>
+          </li>
+          <li class="list_item">
+            <project-list-item></project-list-item>
+          </li>
+          <li class="list_item">
+            <project-list-item></project-list-item>
+          </li>
+          <li class="list_item">
+            <project-list-item></project-list-item>
+          </li>
+          <li class="list_item">
+            <project-list-item></project-list-item>
+          </li>
+          <li class="list_item">
+            <project-list-item></project-list-item>
+          </li>
+          <li class="list_item">
+            <project-list-item></project-list-item>
+          </li>
+          <li class="list_item">
+            <project-list-item></project-list-item>
+          </li>
+          <li class="list_item">
+            <project-list-item></project-list-item>
+          </li>
+          <li class="list_item">
+            <project-list-item></project-list-item>
+          </li>
+          <li class="list_item">
+            <project-list-item></project-list-item>
+          </li>
+          <li class="list_item">
+            <project-list-item></project-list-item>
+          </li>
+        </ul>
+        <v-btn block color="secondary" dark>더 보기</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import DropDown from "./DropDown.vue";
+import ProjectListItem from "./ProjectListItem.vue";
+export default {
+  components: {
+    DropDown,
+    ProjectListItem
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+.info_bar {
+  text-align: center;
+}
+.list_item {
+  margin-bottom: 5px;
+}
+</style>
---
+++ src/views/ProjectListView.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="project_list_section">
+    <h2 class="section_title">프로젝트 리스트</h2>
+    <project-list></project-list>
+  </section>
+</template>
+
+<script>
+import ProjectList from "../components/ProjectListView/ProjectList.vue";
+export default {
+  components: {
+    ProjectList
+  }
+};
+</script>
+
+<style scoped>
+</style>
+
