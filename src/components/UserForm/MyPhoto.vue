5bc33c1af034ee460abab1c7c7b0d5f5db3aba65
KwonYG <andamirocll@gmail.com>
2019-09-11 17:50:13 +0900

    Add: google console meta tag

--- src/components/UserForm/MyPhoto.vue
+++ src/components/UserForm/MyPhoto.vue
@@ -12,7 +12,17 @@
         </div>
         <div class="user_nick">user nick name</div>
         <div class="info_change_btn">
-          <v-btn depressed small>사진 변경</v-btn>
+          <v-btn
+            :loading="loading"
+            :disabled="loading"
+            color="blue-grey"
+            class="ma-2 white--text"
+            @click="loader = 'loading3'"
+          >
+            Upload
+            <v-icon right dark>mdi-cloud-upload</v-icon>
+          </v-btn>
+          <!-- <v-btn depressed small>사진 변경</v-btn> -->
         </div>
       </v-flex>
     </v-layout>
@@ -20,7 +30,24 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      loading: false
+    };
+  },
+
+  watch: {
+    loader() {
+      const l = this.loader;
+      this[l] = !this[l];
+
+      setTimeout(() => (this[l] = false), 3000);
+
+      this.loader = null;
+    }
+  }
+};
 </script>
 
 <style scoped>
