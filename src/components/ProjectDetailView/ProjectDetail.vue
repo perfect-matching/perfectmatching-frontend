6612e1c101fbc9466b64d836bd6233dc499ece61
KwonYG <andamirocll@gmail.com>
2019-09-16 19:56:39 +0900

    Update: ProjectDetailView, add application btn showing logic

--- src/components/ProjectDetailView/ProjectDetail.vue
+++ src/components/ProjectDetailView/ProjectDetail.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="prject_info" grow>
     <v-layout class="project_status_container" align-center justify-center>
-      <v-chip class="status" color="primary" text-color="white">{{
-        project.status
-      }}</v-chip>
+      <v-chip class="status" color="primary" text-color="white">
+        {{ project.status }}
+      </v-chip>
       <div class="create_date">
         {{ setDateFormat(project.startDate) }} -
         {{ setDateFormat(project.endDate) }}
@@ -49,6 +49,9 @@
       </v-btn>
 
       <v-btn
+        v-if="
+          project.leaderIdx !== this.$store.state.myModule.myProfile.userIdx
+        "
         class="apply_btn"
         block
         color="secondary"
